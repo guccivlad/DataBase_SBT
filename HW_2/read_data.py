2179469e2f841aabb14a66cead6cc7f536590b28
Vladislav <shevchenko.vladislav@phystech.edu>
2024-04-04 04:37:50 +0300

    Add Solution

--- HW_2/read_data.py
+++ HW_2/read_data.py
@@ -2,23 +2,6 @@ import redis
 import json
 import time
 
-
-# file = open("trade-indexes.json")
-
-# client = redis.Redis(host='localhost', port=6379, db=0)
-
-# data = json.load(file)
-# data_json_str = json.dumps(data)
-# start_time = time.time()
-
-# client.zadd('zset', {data_json_str : 1})
-
-# end_time = time.time()
-
-# file.close()
-
-# print("Time taken to upload zset: ", end_time - start_time)
-
 client = redis.Redis(host='localhost', port=6379, db=0)
 
 start_time_hset = time.time()
